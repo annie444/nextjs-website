14db456f67a735291587914ba8d713c571f363b9
Annie Ehler <annie.ehler.4@gmail.com>
2022-12-10 21:49:34 -0800

    add my resume content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
 import '../styles/globals.scss'
-import React from 'react';
+import React from 'react'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import ResumeLayout from '../components/ResumeLayout'
+import { MDXProvider } from '@mdx-js/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-	// Use the layout defined at the page level, if available
+  // Use the layout defined at the page level, if available
 
-	return (
-		<>
-			<ChakraProvider>
-				<Component {...pageProps} />
-			</ChakraProvider>
-		</>
-	)
+  return (
+    <>
+      <ChakraProvider>
+        <ResumeLayout>
+          <MDXProvider>
+            <Component {...pageProps} />
+          </MDXProvider>
+        </ResumeLayout>
+      </ChakraProvider>
+    </>
+  )
 }
