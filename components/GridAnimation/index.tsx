dfd40091efda2e69c6b8c9ff3926728d67d14653
Annie Ehler <annie.ehler.4@gmail.com>
2022-12-12 09:24:16 -0800

    Strengthen types for GridAnimation and GridItem

--- components/GridAnimation/index.tsx
+++ components/GridAnimation/index.tsx
@@ -1,59 +1,67 @@
-import { useEffect, useState, useRef, useCallback } from "react";
-import { GridContainer } from "./styles";
-import gridItem from "../GridItem";
-import React from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react'
+import { GridContainer } from './styles'
+import gridItem from '../GridItem'
+import React from 'react'
 
 export default function GridAnimation(): JSX.Element {
+  const gridRef = useRef<HTMLDivElement>(null)
 
-	const gridRef = useRef<HTMLDivElement>(null);
+  const [columns, setColumns] = useState(1)
+  const [rows, setRows] = useState(1)
+  const [total, setTotal] = useState(1)
 
-	const [columns, setColumns] = useState(1);
-	const [rows, setRows] = useState(1);
-	const [total, setTotal] = useState(1);
+  const [GridItems, setGridItems] = useState(
+    Array.from(Array(total)).map((e, i) => {
+      return {
+        id: i,
+        i: i,
+        key: i,
+        columns: columns,
+        rows: rows,
+      }
+    })
+  )
 
-	const [GridItems, setGridItems] = useState(Array.from(Array(total)).map((e, i) => {
-		return {
-			id: i.toString(),
-			i: i,
-			key: i,
-			columns: columns,
-			rows: rows,
-		}
-	}));
+  const createGrid = () => {
+    setColumns(Math.floor(window.innerWidth / 50))
+    setRows(Math.floor(window.innerHeight / 50))
+    setTotal(
+      Math.floor(window.innerWidth / 50) * Math.floor(window.innerHeight / 50)
+    )
+    setGridItems(
+      Array.from(
+        Array(
+          Math.floor(window.innerWidth / 50) *
+            Math.floor(window.innerHeight / 50)
+        )
+      ).map((e, i) => {
+        return {
+          id: i,
+          i: i,
+          key: i,
+          columns: columns,
+          rows: rows,
+        }
+      })
+    )
+  }
 
-	const createGrid = () => {
-		setColumns(Math.floor(window.innerWidth / 50));
-		setRows(Math.floor(window.innerHeight / 50));
-		setTotal(Math.floor(window.innerWidth / 50) * Math.floor(window.innerHeight / 50));
-		setGridItems(
-			Array.from(Array(Math.floor(window.innerWidth / 50) * Math.floor(window.innerHeight / 50))).map((e, i) => {
-				return {
-					id: i.toString(),
-					i: i,
-					key: i,
-					columns: columns,
-					rows: rows,
-				}
-			}));
-	};
+  const memoizedGrid = useCallback(() => {
+    createGrid()
+  }, [])
 
-	const memoizedGrid = useCallback(
-		() => {
-			createGrid();
-		},
-		[],
-	);
+  useEffect(() => {
+    createGrid()
+    window.addEventListener('resize', createGrid)
+  }, [memoizedGrid])
 
-	useEffect(() => {
-		createGrid();
-		window.addEventListener("resize", createGrid);
-	}, [memoizedGrid]);
-
-	return (
-		<>
-			<GridContainer ref={gridRef} columns={columns} rows={rows}>
-				{gridItem({ items: GridItems, columns: columns, rows: rows }).map((item) => item)}
-			</GridContainer>
-		</>
-	);
+  return (
+    <>
+      <GridContainer ref={gridRef} columns={columns} rows={rows}>
+        {gridItem({ items: GridItems, columns: columns, rows: rows }).map(
+          (item) => item
+        )}
+      </GridContainer>
+    </>
+  )
 }
--- components/GridItem/index.tsx
+++ components/GridItem/index.tsx
@@ -1,50 +1,65 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import anime from 'animejs'
 import { GridItem } from './styles'
-import React from 'react';
+import React from 'react'
+import { type MouseEvent } from 'react'
+
+type itemType = {
+  id: number
+  i: number
+  key: number
+  columns: number
+  rows: number
+}
 
 type propsType = {
-	columns: number;
-	rows: number;
-	items: object[];
+  columns: number
+  rows: number
+  items: itemType[]
 }
 
 export default function ItemsList(props: propsType): JSX.Element[] {
+  const [columns, setColumns] = useState<number>(props.columns)
+  const [rows, setRows] = useState<number>(props.rows)
 
-	const [columns, setColumns] = useState<number>(props.columns);
-	const [rows, setRows] = useState<number>(props.rows);
+  const clickHandler = (e: MouseEvent<HTMLDivElement>) => {
+    anime({
+      targets: '.grid-item',
+      scale: [
+        { value: 0.1, easing: 'easeOutSine', duration: 500 },
+        { value: 1, easing: 'easeInOutQuad', duration: 1200 },
+      ],
+      delay: anime.stagger(200, {
+        grid: [columns, rows],
+        from: parseInt(e.currentTarget.id),
+      }),
+    })
+  }
 
-	const clickHandler = (e: any) => {
+  useEffect(() => {
+    setColumns(props.columns)
+    setRows(props.rows)
 
-		const id = e.target.id;
+    anime({
+      targets: '.grid-item',
+      scale: [
+        { value: 0.1, easing: 'easeOutSine', duration: 500 },
+        { value: 1, easing: 'easeInOutQuad', duration: 1200 },
+      ],
+      delay: anime.stagger(200, {
+        grid: [props.columns, props.rows],
+        from: 'center',
+      }),
+    })
+  }, [props.columns, props.rows])
 
-		anime({
-			targets: '.grid-item',
-			scale: [
-				{ value: .1, easing: 'easeOutSine', duration: 500 },
-				{ value: 1, easing: 'easeInOutQuad', duration: 1200 }
-			],
-			delay: anime.stagger(200, { grid: [columns, rows], from: id })
-		});
-	}
-
-	useEffect(() => {
-
-		setColumns(props.columns);
-		setRows(props.rows);
-
-		anime({
-			targets: '.grid-item',
-			scale: [
-				{ value: .1, easing: 'easeOutSine', duration: 500 },
-				{ value: 1, easing: 'easeInOutQuad', duration: 1200 }
-			],
-			delay: anime.stagger(200, { grid: [props.columns, props.rows], from: 'center' })
-		});
-
-	}, [props.columns, props.rows]);
-
-	return Array.from(props.items).map((item: any) => (
-		<GridItem id={item.id} key={item.i} onClick={clickHandler} style={{ backgroundColor: '#fff' }} className="grid-item"></GridItem>
-	));
+  return Array.from(props.items).map((item: itemType) => (
+    <GridItem
+      id={item.id.toString()}
+      key={item.i}
+      onClick={clickHandler}
+      style={{ backgroundColor: '#fff' }}
+      className="grid-item"
+    ></GridItem>
+  ))
 }
