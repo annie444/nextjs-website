d69833d069390e83cc76c30b8d98ad8e630bf19d
Annie Ehler <annie.ehler.4@gmail.com>
2022-12-14 14:47:22 -0800

    markdown files... lemme tell ya

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,20 @@
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		"next",
-		"next/core-web-vitals",
-		"plugin:react/recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:prettier/recommended",
-		"plugin:mdx/recommended"
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: [
-		'@typescript-eslint',
-		'react-hooks',
-		'react',
-		'mdx',
-		'prettier'
-	],
-	"rules": {
-		"prefer-const": "error",
-		"@typescript-eslint/no-unused-vars": "error",
-		"@typescript-eslint/no-explicit-any": "error",
-		"prettier/prettier": "error"
-	}
-};
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next',
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:mdx/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks', 'react', 'mdx', 'prettier'],
+  rules: {
+    'prefer-const': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'prettier/prettier': 'error',
+  },
+}
