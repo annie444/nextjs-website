a6509f64ab405cd0529d6f21175dc1d730e60155
Annie Ehler <annie.ehler.4@gmail.com>
2022-12-15 16:44:08 -0800

    better seo lol

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,6 +1,4 @@
-import fetch from 'isomorphic-unfetch'
 import { NextApiResponse } from 'next'
-//import Cors from 'cors'
 
 const url = 'https://annieehler.com'
 
@@ -23,21 +21,52 @@ export const getServerSideProps = async ({ res }: { res: NextApiResponse }) => {
 }
 
 function generateSiteMap(posts: { slug: string }[]) {
+  const today = new Date()
+  const dd = String(today.getDate()).padStart(2, '0')
+  const mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
+  const yyyy = today.getFullYear()
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
-     </url>
+		 <url>
+		 				 <loc>${url}</loc>
+						 <lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
+						 <changefreq>daily</changefreq>
+						 <priority>1</priority>
+			</url>
+			<url>
+					<loc>${url}/sitemap.xml</loc>
+					<lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
+					<changefreq>daily</changefreq>
+					<priority>4</priority>
+			</url>
+			<url>
+					<loc>${url}/cache/resume.json</loc>
+					<lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
+					<changefreq>monthly</changefreq>
+					<priority>5</priority>
+			</url>
+			<url>
+					<loc>${url}/api/resume</loc>
+					<lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
+					<changefreq>monthly</changefreq>
+					<priority>4</priority>
+			</url>
      ${posts
        .map(({ slug }: { slug: string }) => {
          return `
        <url>
            <loc>${`${url}/${slug}`}</loc>
+					 <lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
+					 <changefreq>daily</changefreq>
+					 <priority>2</priority>
        </url>
+			 <url>
+			 		<loc>${`${url}/api/${slug}`}</loc>
+					<lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
+					<changefreq>monthly</changefreq>
+					<priority>3</priority>
+	 		</url>
      `
        })
        .join('')}
