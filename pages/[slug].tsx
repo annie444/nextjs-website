dbed829baaf51c9ef4737b314f41f5f68b9ee567
Annie Ehler <annie.ehler.4@gmail.com>
2022-12-15 16:58:43 -0800

    More seo

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styles from '../styles/layout.module.css'
 import { Navbar } from '../components/NavBar'
 import { MDXProvider } from '@mdx-js/react'
+import Head from 'next/head'
 
 const url = 'https://annieehler.com'
 
@@ -13,7 +14,12 @@ interface Params {
 interface Post {
   slug: string
   content: string
-  data: object
+  data: {
+    title: string
+    description: string
+    date: string
+    author: string
+  }
   excerpt: string
   file: {
     cwd: string
@@ -38,6 +44,38 @@ interface Page {
 export default function Page({ post, pages }: { post: Post; pages: Page[] }) {
   return (
     <>
+      <Head>
+        <title>{post.data.title}</title>
+        <meta name="description" content={post.data.description} />
+        <meta name="author" content={post.data.author} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@anniethetrannie" />
+        <meta name="twitter:creator" content="@anniethetrannie" />
+        <meta property="og:url" content={`${url}/resume/${post.slug}`} />
+        <meta property="og:title" content={post.data.title} />
+        <meta property="og:description" content={post.data.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Annie Ehler" />
+        <meta property="og:updated_time" content={post.data.date} />
+        <meta property="og:published_time" content={post.data.date} />
+        <meta property="og:author" content={post.data.author} />
+        <meta property="og:article:author" content={post.data.author} />
+        <meta property="og:article:published_time" content={post.data.date} />
+        <meta property="og:article:modified_time" content={post.data.date} />
+        <meta property="og:article:section" content={post.data.title} />
+        <meta property="og:article:tag" content="Resume" />
+        <meta property="og:article:tag" content="Annie Ehler" />
+        <meta property="og:article:tag" content="Annie" />
+        <meta property="og:article:tag" content="Ehler" />
+        <meta property="og:article:tag" content="Annie Ehler Resume" />
+        <meta property="og:article:tag" content="Annie Ehler Portfolio" />
+        <meta
+          property="og:article:tag"
+          content="Annie Ehler Portfolio Resume"
+        />
+      </Head>
       <div className={styles['main-area']} id="MainArea">
         <MDXProvider>
           <MDXRemote {...(post?.mdx ?? post?.source)} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import fetch from 'isomorphic-unfetch'
 import styles from '../styles/layout.module.css'
 import { Navbar } from '../components/NavBar'
+import Head from 'next/head'
 
 const url = 'https://annieehler.com'
 
@@ -16,6 +17,56 @@ interface Post {
 export default function Page({ pages }: { pages: Post[] }) {
   return (
     <>
+      <Head>
+        <title>Analetta Rae Marie Ehler</title>
+        <meta
+          name="description"
+          content="Analetta (Annie) Rae Marie Ehler Bioinformatics Data Science Portfolio and Resume"
+        />
+        <meta name="author" content="Analetta (Annie) Ehler" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@anniethetrannie" />
+        <meta name="twitter:creator" content="@anniethetrannie" />
+        <meta property="og:url" content={`${url}`} />
+        <meta property="og:title" content="Analetta Rae Marie Ehler" />
+        <meta
+          property="og:description"
+          content="Analetta (Annie) Rae Marie Ehler Bioinformatics Data Science Portfolio and Resume"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Annie Ehler" />
+        <meta
+          property="og:updated_time"
+          content={`${new Date().getUTCFullYear()}-${new Date().getUTCMonth()}-${new Date().getUTCDate()}`}
+        />
+        <meta
+          property="og:published_time"
+          content={`${new Date().getUTCFullYear()}-${new Date().getUTCMonth()}-${new Date().getUTCDate()}`}
+        />
+        <meta property="og:author" content="Analetta (Annie) Ehler" />
+        <meta property="og:article:author" content="Analetta (Annie) Ehler" />
+        <meta
+          property="og:article:published_time"
+          content={`${new Date().getUTCFullYear()}-${new Date().getUTCMonth()}-${new Date().getUTCDate()}`}
+        />
+        <meta
+          property="og:article:modified_time"
+          content={`${new Date().getUTCFullYear()}-${new Date().getUTCMonth()}-${new Date().getUTCDate()}`}
+        />
+        <meta property="og:article:section" content="home" />
+        <meta property="og:article:tag" content="Resume" />
+        <meta property="og:article:tag" content="Annie Ehler" />
+        <meta property="og:article:tag" content="Annie" />
+        <meta property="og:article:tag" content="Ehler" />
+        <meta property="og:article:tag" content="Annie Ehler Resume" />
+        <meta property="og:article:tag" content="Annie Ehler Portfolio" />
+        <meta
+          property="og:article:tag"
+          content="Annie Ehler Portfolio Resume"
+        />
+      </Head>
       <div className={styles['main-area']} id="MainArea"></div>
       <div className="nav-area" id="NavBar">
         <Navbar pages={pages} />
