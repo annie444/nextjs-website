5569d937f658fd5a42dda81358b3c74198d5cf74
Annie Ehler <annie.ehler.4@gmail.com>
2022-12-17 13:49:01 -0800

    improve SEO

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,77 +1,13 @@
-export const config = {
-  runtime: 'experimental-edge',
-}
-
-const url = 'https://annieehler.com'
+const url = 'http://localhost:3000'
 
 export const getServerSideProps = async () => {
   // We make an API call to gather the URLs for our site
-  // Fetch our JSON file
-  const posts = await fetch(`${url}/cache/resume.json`).then((res) =>
-    res.json()
-  )
-  // Do something with your data!
-  const sitemap = generateSiteMap(posts)
-  return new Response(sitemap, {
-    status: 200,
-    headers: {
-      ContentType: 'text/xml',
+  return {
+    redirect: {
+      destination: `${url}/api/sitemap.xml`,
+      statusCode: 302,
     },
-  })
-}
-
-function generateSiteMap(posts: { slug: string }[]) {
-  const today = new Date()
-  const dd = String(today.getDate()).padStart(2, '0')
-  const mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
-  const yyyy = today.getFullYear()
-  return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-		 <url>
-		 				 <loc>${url}</loc>
-						 <lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
-						 <changefreq>daily</changefreq>
-						 <priority>1</priority>
-			</url>
-			<url>
-					<loc>${url}/sitemap.xml</loc>
-					<lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
-					<changefreq>daily</changefreq>
-					<priority>4</priority>
-			</url>
-			<url>
-					<loc>${url}/cache/resume.json</loc>
-					<lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
-					<changefreq>monthly</changefreq>
-					<priority>5</priority>
-			</url>
-			<url>
-					<loc>${url}/api/resume</loc>
-					<lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
-					<changefreq>monthly</changefreq>
-					<priority>4</priority>
-			</url>
-     ${posts
-       .map(({ slug }: { slug: string }) => {
-         return `
-       <url>
-           <loc>${`${url}/${slug}`}</loc>
-					 <lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
-					 <changefreq>daily</changefreq>
-					 <priority>2</priority>
-       </url>
-			 <url>
-			 		<loc>${`${url}/api/${slug}`}</loc>
-					<lastmod>${`${yyyy}-${mm}-${dd}`}</lastmod>
-					<changefreq>monthly</changefreq>
-					<priority>3</priority>
-	 		</url>
-     `
-       })
-       .join('')}
-   </urlset>
- `
+  }
 }
 
 const SiteMap = () => {
